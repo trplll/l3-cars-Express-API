a73cc41b11a8e1e018688e3602e560beac9928d2
Luke <trplll@users.noreply.github.com>
2017-06-28 13:25:22 -0500

    add cardb.js for sample data
    Allows users to import sample data into mongoDB

---
+++ cardb.js
@@ -0,0 +1,20 @@
+db.cars.insert([
+    {
+	'brand': 'Ford',
+    'model': 'Mustang GT',
+    'engine': '5.0L V8',
+    'horsepower': 435
+	},
+	{
+	'brand': 'Ford',
+    'model': 'F150 XLT',
+    'engine': '5.0L V8',
+    'horsepower': 411
+	},
+	{
+	'brand': 'Mitsubishi',
+    'model': 'Lancer',
+    'engine': '2.0L four-cylinder',
+    'horsepower': 148
+	}
+])
